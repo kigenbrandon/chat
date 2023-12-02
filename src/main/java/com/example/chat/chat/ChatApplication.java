56031064d1e1ee7e11449c4473d3138d18cef1e5
kigenbrandon <kigenbrandon@proton.me>
2023-12-02 05:53:17 +0300

    testing the application with command line runner

--- src/main/java/com/example/chat/chat/ChatApplication.java
+++ src/main/java/com/example/chat/chat/ChatApplication.java
@@ -1,8 +1,10 @@
 package com.example.chat.chat;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ChatApplication {
@@ -10,5 +12,10 @@ public class ChatApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ChatApplication.class, args);
 	}
-
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return (a)->{
+			System.out.println("dddd");
+		};
+	}
 }
