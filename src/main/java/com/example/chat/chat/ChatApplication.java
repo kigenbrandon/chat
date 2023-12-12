e49d04acdc8dbfa663dfe5d9d3d7ab4ebd0ac4a7
kigenbrandon <kigenbrandon@proton.me>
2023-12-12 18:03:12 +0300

    adding file upload endpoint

--- src/main/java/com/example/chat/chat/ChatApplication.java
+++ src/main/java/com/example/chat/chat/ChatApplication.java
@@ -1,21 +1,46 @@
 package com.example.chat.chat;
 
+import com.example.chat.chat.config.StorageProperties;
+import com.example.chat.chat.service.StorageService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableConfigurationProperties(StorageProperties.class)
 public class ChatApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatApplication.class, args);
 	}
 	@Bean
-	public CommandLineRunner commandLineRunner() {
-		return (a)->{
-			System.out.println("dddd");
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
 		};
+	}
+	@Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin","Access-Control-Allow-Origin","Content-Type","Accept","Authorization","Origin","Accept","X-Request-With","Access-Control-Request-Method","Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin","Content-Type","Accept","Authorization","Access-Control-Allow-Origin","Access-Control-Allow-Credentials"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET","PUT","POST","DELETE","OPTIONS"));
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource =new UrlBasedCorsConfigurationSource();
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**",corsConfiguration);
+
+
+		return new CorsFilter(urlBasedCorsConfigurationSource);
 	}
 }
--- src/test/java/com/example/chat/chat/ChatApplicationTests.java
+++ src/test/java/com/example/chat/chat/ChatApplicationTests.java
@@ -1,13 +1,29 @@
 package com.example.chat.chat;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
 @SpringBootTest
 class ChatApplicationTests {
+	private static TestRestTemplate template;
+	private static String baseUrl= "";
+	@BeforeAll
+	private static void init(){
+		template = new TestRestTemplate();
+
+	}
 
 	@Test
 	void contextLoads() {
+
+	}
+
+	@Test
+	void testWs() {
+
+
 	}
 
 }
